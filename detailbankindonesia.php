f8987f10086877b6236766770ff7db3602168a0d
sarahcantika <83532698+sarahcantika@users.noreply.github.com>
2022-06-19 19:41:50 +0700

    Add files via upload

---
+++ detailbankindonesia.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,500&display=swap" rel="stylesheet">
+	<title></title>
+</head>
+<body background="#30336b">
+<table>
+	<div class="logo"><img width="450" src="bankindonesia.png"></div>
+		<div class="contents">
+			<p>Beasiswa Bank Indonesia adalah beasiswa dari Bank Indonesia (BI) untuk mahasiswa S-1, D-4, dan D-3 di berbagai PTN dan PTS terpilih. Melalui program beasiswa ini, BI memberikan bantuan biaya kuliah, tunjangan studi, dan biaya hidup untuk GenBI (sebutan untuk penerima beasiswa).</p>
+				Adapun nilai bantuan uang bagi penerima Beasiswa Bank Indonesia adalah sebagai berikut :
+				<ul>
+				    <li>Rp1.500.000/bulan untuk penerima Beasiswa Unggulan S-1;</li>
+			        <li>Rp1.000.000/bulan untuk penerima Beasiswa Reguler S-1;</li>
+			        <li>Beasiswa Reguler D-4: Rp1.000.000/bulan; dan</li>
+			        <li>Beasiswa Reguler D-3: Rp1.000.000/bulan.</li>
+			    </ul>
+			 Adapun kriteria penerimaan Beasiswa Bank Indonesia adalah sebagai berikut :
+			 <ul>
+			 	<li>Merupakan mahasiswa aktif program S-1, D-4, dan D-3 yang dibuktikan dengan kartu tanda mahasiswa atau surat keterangan aktif;</li>
+			 	<li>Sekurang-kurangnya telah menyelesaikan 40 SKS (S-1) serta 24 SKS (D-4 dan D-3);</li>
+			 	<li>Setiap mahasiswa menerima beasiswa paling lama hanya empat semester (S-1) dan dua semester (D-4 dan D-3) sepanjang memenuhi kriteria yang ada;</li>
+			 	<li>Maksimal berusia 23 tahun atau belum berusia 24 tahun (S-1) dan maksimal berusia 22 tahun atau belum berusia 23 tahun (D-4 dan D-3) pada saat penetapan penerima beasiswa. Sementara itu, untuk mahasiswa Orang Asli Papua (OAP) di Papua dan Papua Barat, batas usia bisa sampai 27 tahun;</li>
+			 	<li>Memiliki Indeks Prestasi Kumulatif (IPK) semester terakhir minimal 3.5 (Beasiswa S-1 Unggulan), IPK minimal 3.00 (Beasiswa S-1 Reguler, D-4, dan D-3);</li>
+			 	<li>Memiliki TOEFL Score (PBT atau ITP) sebesar 500; atau TOEFL iBT sebesar 61; atau TOEFL CBT sebesar 173; ataupun IELTS sebesar 6.0 dengan melampirkan dokumen pendukung (Khusus Beasiswa S-1 Unggulan);</li>
+			 	<li>Membuat motivation letter dan rencana karier setelah menyelesaikan studi sarjana dalam bahasa Inggris (Beasiswa S-1 Unggulan) serta dalam bahasa Indonesia (Beasiswa S-1 Reguler, D-4, dan D-3);</li>
+			 	<li>Menyertakan surat rekomendasi dari 2 tokoh (S-1 unggulan) dan 1 tokoh (S-1 Reguler, D-4, dan D-3 (akademik dan nonakademik);</li>
+			 	<li>Tidak sedang menerima beasiswa dari lembaga lain dan/atau berada dalam status ikatan dinas dari lembaga/instansi lain;</li>
+			 	</ul> 
+		</div>
+</table>
+
+<div class="col d-flex justify-content-center">
+	<table>
+	<tr>
+    <td>
+      <center><a href="beranda.php" class="btn btn-warning">Kembali</a></center>
+    </td>
+    <td></td>
+    <td>
+      <center><a href="tambah_data.php" class="btn btn-warning">Daftar</a></center>
+    </td>
+  </tr>
+  </table>
+</div>
+	</body>
+</html>
